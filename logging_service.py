3a04f809767afe009121467e1fb73f74722960f0
Emanuel Metzenthin <emanuel.metzenthin@student.hpi.de>
2021-05-02 20:00:20 +0200

    Debug print in logger

--- logging_service.py
+++ logging_service.py
@@ -7,11 +7,11 @@ import ray
 @ray.remote
 class LoggingService:
     def __init__(self, cfg):
-        self.current_parameters = None
-
         self.run = neptune.init(run=cfg.neptune.run_id, api_token=cfg.neptune.key, project='emanuelm/rl-scene-text-detection', name=cfg.neptune.run_name)
         neptune_dict = json.loads(str(cfg).replace("\'", '"').replace('True', "true").replace("False", "false").replace("None", "null"))
         self.run['parameters'] = neptune_dict
 
     def log(self, name, value):
+        print(name)
+        print(value)
         self.run[name].log(value)
