93f1398df3b6f35fcd05ae9ea5c3fc4a5f28637f
Emanuel Metzenthin <emanuel.metzenthin@student.hpi.de>
2021-05-03 14:18:06 +0200

    Fix

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ def main(cfg: DictConfig):
     dqn = ImageDQN(num_actions=env.action_space.n)
     target_dqn = ImageDQN(num_actions=env.action_space.n)
 
-    replay_buffer = ReplayBuffer.options(name="replay_buffer", lifetime="detached").remote(cfg)
-    param_server = ParameterServer.options(name="param_server", lifetime="detached").remote()
+    ReplayBuffer.options(name="replay_buffer", lifetime="detached").remote(cfg)
+    ParameterServer.options(name="param_server", lifetime="detached").remote()
     learner = Learner.remote(dqn, target_dqn, logger, cfg)
     actors = [Actor.remote(deepcopy(dqn), deepcopy(target_dqn), deepcopy(env), learner, logger, cfg, i) for i in range(cfg.apex.num_actors)]
 
