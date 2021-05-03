7bd354e8a12f489a9cfbfdc1a64791740ad1fb0c
Emanuel Metzenthin <emanuel.metzenthin@student.hpi.de>
2021-05-03 11:49:27 +0200

    more print debug

--- parameter_server.py
+++ parameter_server.py
@@ -7,9 +7,12 @@ class ParameterServer:
         self.current_parameters = None
 
     def publish_parameters(self, object_ref):
+        print(f"published params 1")
         if self.current_parameters:
             del self.current_parameters
+        print(f"published params 2")
         self.current_parameters = object_ref
+        print(f"published params 3 {self.current_parameters})")
 
     def get_current_parameters(self):
         print(f"current params: {self.current_parameters}")
