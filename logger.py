32f74ef52ffb35431a9c90c40667a198a25e5def
Emanuel Metzenthin <emanuel.metzenthin@student.hpi.de>
2021-05-03 18:56:23 +0200

    logger

---
+++ logger.py
@@ -0,0 +1,83 @@
+import neptune
+import numpy as np
+from ray import tune
+
+
+class NeptuneLogger(tune.logger.Logger):
+    """RLlib Neptune logger.
+    Example usage:
+    ```
+    import ray
+    from ray import tune
+    ray.init()
+    tune.run(
+        "PPO",
+        stop={"episode_reward_mean": 200},
+        config={
+            "env": "CartPole-v0",
+            "num_gpus": 0,
+            "num_workers": 1,
+            "lr": tune.grid_search([0.01, 0.001, 0.0001]),
+            "logger_config": {"neptune_project_name": '<user name>/sandbox'},
+        },
+        loggers=tune.logger.DEFAULT_LOGGERS + (NeptuneLogger,),
+    )
+    ```
+    """
+
+    def _init(self):
+        logger_config = self.config.get('logger_config')
+        neptune.init(logger_config.get('neptune_project_name'))
+        self.neptune_experiment = neptune.create_experiment(
+            name=str(self.trial),  # Gets the trial name.
+            params=self.config,
+        )
+
+    @staticmethod
+    def dict_multiple_get(dict_, indices):
+        """Access the nested value."""
+        if not isinstance(indices, list):
+            indices = [indices]
+
+        value = dict_
+        index = None
+        for index in indices:
+            try:
+                value = value[index]
+            except KeyError:
+                print("Skipping", indices)
+                return {}
+
+        if isinstance(value, dict):
+            return value
+        else:
+            return {index: value}
+
+    def on_result(self, result):
+        list_to_traverse = [
+            [],
+            ['custom_metrics'],
+            ['evaluation'],
+            ['info', 'num_steps_trained'],
+            ['info', 'learner'],
+            ['info', 'exploration_infos', 0],
+            ['info', 'exploration_infos', 1],
+            ['info', 'learner', "default_policy"]
+        ]
+
+        for indices in list_to_traverse:
+            res_ = self.dict_multiple_get(result, indices)
+            prefix = '/'.join([str(idx) for idx in indices])
+            for key, value in res_.items():
+                prefixed_key = '/'.join([prefix, key])
+                if isinstance(value, float) or isinstance(value, int):
+                    self.neptune_experiment.log_metric(
+                        prefixed_key, value)
+                elif (isinstance(value, np.ndarray) or
+                      isinstance(value, np.number)):
+                    self.neptune_experiment.log_metric(
+                        prefixed_key, float(value))
+                # Otherwise ignore
+
+    def close(self):
+        neptune.stop()
--- rllib_train.py
+++ rllib_train.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 import hydra
 import ray
@@ -6,12 +7,12 @@ from ray import tune
 from ray.rllib import agents
 from ray.rllib.models import ModelCatalog
 from ray.tune import register_env
-
 from DQN import RLLibImageDQN
 from env_factory import EnvFactory
+from logger import NeptuneLogger
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--run", type=str, default="IMPALA")
+parser.add_argument("--run", type=str, default="APEX")
 parser.add_argument("--torch", action="store_true")
 parser.add_argument("--as-test", action="store_true")
 parser.add_argument("--stop-iters", type=int, default=50)
@@ -22,19 +23,19 @@ parser.add_argument("--stop-reward", type=float, default=0.1)
 @hydra.main(config_path="cfg", config_name="config.yml")
 def main(cfg):
     # env = EnvFactory.create_env("sign", cfg)
-
     ModelCatalog.register_custom_model("imagedqn", RLLibImageDQN)
     register_env("textloc", lambda config: EnvFactory.create_env("sign", cfg))
     config = {
         "env": "textloc",
         # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
         "num_gpus": 0,
-        "model": {
-            "custom_model": "imagedqn",
-        },
+        # "model": {
+        #     "custom_model": "imagedqn",
+        # },
         "lr": 1e-4,  # try different lrs
         "num_workers": 1, #cfg.apex.num_actors,  # parallelism
         "framework": "torch",
+        "logger_config": {"neptune_project_name": "emanuelm/rl-scene-text-detection"}
     }
 
     stop = {
@@ -43,7 +44,7 @@ def main(cfg):
         "episode_reward_mean": args.stop_reward,
     }
 
-    results = tune.run(args.run, config=config, stop=stop)
+    results = tune.run(args.run, config=config, stop=stop, loggers=tune.logger.DEFAULT_LOGGERS + (NeptuneLogger,),)
 
     ray.shutdown()
 
