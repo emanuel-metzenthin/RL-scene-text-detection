57812e1a2c878d2d8bd28db44ed4dbb5e72ddf84
Emanuel Metzenthin <emanuel.metzenthin@student.hpi.de>
2021-05-18 13:22:04 +0200

    Init assessor weights normal dist.

--- assessor.py
+++ assessor.py
@@ -91,11 +91,13 @@ class AssessorModel(nn.Module):
             ResBlock2(),
             ResBlock3(),
             ResBlock3(),
+            nn.MaxPool2d(12),
+            nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(401408, 1),
+            nn.Linear(2048, 1),
         )
         self.relu = nn.ReLU()
-        # self.resnet.apply(self.init_weights)
+        self.resnet.apply(self.init_weights)
 
     def forward(self, X):
         # feat = self.feature_extractor(X)
@@ -107,7 +109,7 @@ class AssessorModel(nn.Module):
     @staticmethod
     def init_weights(m):
         if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
-            torch.nn.init.xavier_uniform_(m.weight)
+            torch.nn.init.normal(m.weight, 0.02)
 
 
 def train():
