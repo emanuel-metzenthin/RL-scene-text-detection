88e3a40c80b294965ec473ceae2c66375ad8b61b
emanuel <emanuel.metzenthin@student.hpi.de>
2021-05-24 11:02:59 +0200

    Fix datasets

--- assessor.py
+++ assessor.py
@@ -102,7 +102,7 @@ class AssessorModel(nn.Module):
             nn.AdaptiveAvgPool2d(4),
             nn.Flatten(),
             nn.Linear(4096, 1),
-            # nn.Sigmoid()
+            #nn.Sigmoid()
         )
         self.resnet.apply(self.init_weights)
 
@@ -124,10 +124,10 @@ def train():
     model = AssessorModel()
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model.to(device)
-    # model.load_state_dict(torch.load('assessor_model.pt'))
+    #model.load_state_dict(torch.load('assessor_model.pt'))
 
-    train_data = AssessorDataset('../data/assessor_data/train')
-    val_data = AssessorDataset('../data/assessor_data/val', split="val")
+    train_data = AssessorDataset('/mnt/ssd/emanuel/data/iou_samples_larger')
+    val_data = AssessorDataset('/home/emanuel/data/assessor_data2/val', split="val")
     train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
     val_loader = DataLoader(val_data, batch_size=32)
 
--- dataset/assessor_dataset.py
+++ dataset/assessor_dataset.py
@@ -8,8 +8,8 @@ import pandas as pd
 class AssessorDataset(Dataset):
     def _load_images_and_gt(self):
         self.images = []
-        # img_files = open(os.path.join(self.path, 'image_locations.txt')).readlines()
-        # self.gt = np.load(os.path.join(self.path, 'ious.npy'), allow_pickle=True)
+        #img_files = open(os.path.join(self.path, 'image_locations.txt')).readlines()
+        #self.gt = np.load(os.path.join(self.path, 'ious.npy'), allow_pickle=True)
         df = pd.read_csv(os.path.join(self.path, 'images.csv'), header=None, sep='\t')
         img_files = df[0]
         self.gt = df[1]
