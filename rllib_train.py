da9065f813289a7ed6dd9b05fb14b0e2a495f47b
Emanuel Metzenthin <emanuel.metzenthin@student.hpi.de>
2021-05-31 11:54:22 +0200

    Add eval and random seed

--- rllib_train.py
+++ rllib_train.py
@@ -60,10 +60,10 @@ def main(cfg):
         "learning_starts": 0,
         "framework": "torch",
         "compress_observations": True,
-        "render_env": True,
+        "render_env": False,
         "logger_config": cfg,
         "observation_filter": lambda x: NormalizeFilter(),
-        "seed": cfg.training.random_seed
+        "seed": cfg.training.random_seed,
     }
 
     stop = {
