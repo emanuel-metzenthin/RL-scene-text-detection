24a250c1b05d1b5a813e949573e1f6a5db979857
emanuel <emanuel.metzenthin@student.hpi.de>
2021-05-31 14:15:51 +0200

    Fix

--- scripts/simple_dataset_to_icdar_gt.py
+++ scripts/simple_dataset_to_icdar_gt.py
@@ -8,12 +8,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument("data_path")
 args = parser.parse_args()
 
-bboxes = np.load(os.join(args.data_path, "bounding_boxes.npy"))
+bboxes = np.load(os.path.join(args.data_path, "bounding_boxes.npy"))
 os.mkdir("./simple_gt")
 zipf = zipfile.ZipFile('./simple_gt/simple_gt.zip', 'w', zipfile.ZIP_DEFLATED)
 
 for i, bb_list in enumerate(bboxes):
-    test_file = open(f'./results/res_img_{i}.txt', 'w+')
+    test_file = open(f'./simple_gt/res_img_{i}.txt', 'w+')
 
     for bb in bb_list:
         test_file.write(f"{','.join(map(str, bb))}\n")
