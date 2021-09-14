38f328c3ba225530c85e41e1eb10e08c96810c6f
Emanuel Metzenthin <emanuel.metzenthin@student.hpi.de>
2021-09-14 11:13:11 +0200

    Add repeat penalty

--- env_factory.py
+++ env_factory.py
@@ -50,6 +50,7 @@ class EnvFactory:
             ior_marker_type='cross',
             has_termination_action=cfg.env.termination,
             has_intermediate_reward=cfg.reward.intermediate_reward,
+            has_repeat_penalty=cfg.reward.repeat_penalty,
             assessor_model=assessor_model,
             train_assessor=assessor_data is not None,
             grayscale=framestacking_mode == 'grayscale',
