e8e0458cda50f0f7c05006c2bfd136c05f8785fe
Emanuel Metzenthin <emanuel.metzenthin@student.hpi.de>
2021-11-12 16:19:13 +0100

    Fix

--- dataset/LSVT_dataset.py
+++ dataset/LSVT_dataset.py
@@ -7,4 +7,4 @@ class LSVTDataset(Dataset):
         file_names = [i for i in os.listdir(self.path) if i.lower()[-4:] in ['.jpg', '.png', '.gif']]
         file_names.sort(key=lambda x: int(x.split('.')[0].split('_')[-1]))
         self.images = [os.path.join(self.path, i) for i in file_names]
-        self.gt = []
+        self.gt = [[] for _ in self.images]
