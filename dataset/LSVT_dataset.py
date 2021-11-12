1e5e104e6f94abf5b7af7ccb294c1dbd6ced48ae
Emanuel Metzenthin <emanuel.metzenthin@student.hpi.de>
2021-11-12 16:14:18 +0100

    Fix path

--- dataset/LSVT_dataset.py
+++ dataset/LSVT_dataset.py
@@ -6,5 +6,5 @@ class LSVTDataset(Dataset):
     def _load_images_and_gt(self):
         file_names = [i for i in os.listdir(self.path) if i.lower()[-4:] in ['.jpg', '.png', '.gif']]
         file_names.sort(key=lambda x: int(x.split('.')[0].split('_')[-1]))
-        self.images = [os.path.join(folder, i) for i in file_names]
+        self.images = [os.path.join(self.path, i) for i in file_names]
         self.gt = []
